ea812456b82ddbfc7b654d953b95d4b86b077471
gabelorenzo <gtlorenzo93@gmail.com>
2021-01-25 03:20:10 -0500

    Improved entity-to-dto example in the driver class

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,7 +1,14 @@
 import dto.MovieDto;
+import entity.AddressEntity;
+import entity.DirectorEntity;
 import entity.MovieEntity;
 import mapper.MovieMapper;
+import types.Country;
+import types.Language;
+import types.Rating;
+import types.State;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.UUID;
 
@@ -12,12 +19,30 @@ public class Application {
         MovieEntity entity = MovieEntity.builder()
                 .id(movieId)
                 .title("Jaws")
-                .durationMinutes(120)
-                .releaseDate(LocalDate.of(1980, 5, 3))
+                .releaseDate(LocalDate.of(1975, 6, 20))
+                .budget(BigDecimal.valueOf(7000000.00))
+                .synopsis("Shark eat ppl?")
+                .rating(Rating.PG)
+                .supportedLanguages(new Language[] { Language.ENGLISH, Language.FRENCH })
+                .director(
+                        DirectorEntity.builder()
+                                .id(UUID.fromString("8e11816a-5192-4234-82c7-fe67d6da3fd5"))
+                                .firstName("Steven")
+                                .lastName("Spielberg")
+                                .dob(LocalDate.of(1946, 12, 18))
+                                .build()
+                )
+                .address(
+                        AddressEntity.builder()
+                                .state(State.CALIFORNIA)
+                                .country(Country.UNITED_STATES)
+                                .build()
+                )
+                .durationMinutes(124)
                 .build();
 
         MovieDto dto = MovieMapper.INSTANCE.entityToDto(entity);
 
-        System.out.println(dto.getTitle());
+        System.out.println(dto);
     }
 }
